2a0584a588c08a2b7f328757819c3b646b1d538d
Robert Dougan <rdougan@me.com>
2014-10-10 20:12:51 +0200

    Adding yosemite helper method and using highlighted image for statusbar icon

--- Classes/Utilities.h
+++ Classes/Utilities.h
@@ -5,6 +5,9 @@ NSString* unitStringFromSeconds(NSTimeInterval interval);
 NSString* unitStringFromBytes(double bytes);
 NSString* unitStringFromBytes2(double bytes, uint8_t flags);
 
+BOOL leopardOrGreater(void);
+BOOL yosemiteOrGreater(void);
+
 enum {
     kUnitStringBinaryUnits     = 1 << 0,
     kUnitStringOSNativeUnits   = 1 << 1,
